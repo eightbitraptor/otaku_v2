5bb6c5abf9f2f999186fb10a458b2fb148606cff
Matt Valentine-House <matt@eightbitraptor.com>
2018-05-25 17:40:20 +0100

    Introduce XDG for config and data dirs
    In order to configure the application and to provide a place to store
    catalogue data about the things that are being downloaded, we need to
    find out where the best place on the system to store files is.
    
    This commit pulls in the XDG crate so that we can use the Free Desktop
    specification.
    
    TODO: I have no idea at this point whether any of this is tenable on
    Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 extern crate base64;
+extern crate xdg;
 
 mod downloader;
 
 fn main() {
+    let base_dirs = xdg::BaseDirectories::with_prefix("otaku")
+        .expect("could not find XDG Base Directory settings");
+
+    let catalogue_db_path = base_dirs
+        .place_data_file("catalogue.sqlite")
+        .expect("could not get data path");
+
     downloader::download_image(
         "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-        "/tmp"
+        "/tmp",
     );
 }
